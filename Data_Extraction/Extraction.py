1128e0b26266391d3b3ee0df65fc7793669ebd38
OtnielGomes <OtnielGomes>
2024-02-18 12:33:57 +0000

    2024-02-18

--- Data_Extraction/Extraction.py
+++ Data_Extraction/Extraction.py
@@ -1,10 +1,15 @@
 # Databricks notebook source
-# MAGIC %md
-# MAGIC ## Extrating Data
+dbutils.widgets.text("data_execucao", "")
+data_execucao = dbutils.widgets.get("data_execucao")
+
 
 # COMMAND ----------
 
 import requests
+from pyspark.sql.functions import lit
+
+
+# COMMAND ----------
 
 def extrat_data(date, base = 'BRL'):
   
@@ -25,65 +30,41 @@ def extrat_data(date, base = 'BRL'):
 
 # COMMAND ----------
 
-extrat_data('2022-09-07', base = 'BRL')
+def save_parquet(extracted_conversions):
+    
+    # Extracting date from records
+    year, month, day = extracted_conversions['date'].split('-')
+
+    # Defining the path 
+    path = f'/Workspace/Repos/202307063411@alunos.estacio.br/Pipelines_with_Airflow_and_Azure-Databricks/data/bronze/{year}/{month}/{day}'
+
+    # Saving data to a variable
+    response = [(coin, float(rate)) for coin, rate in extracted_conversions['rates'].items()]
+
+    # Creating DataFrame
+    df_conversions = spark.createDataFrame(response, schema = ['coin', 'rate'])
+
+    # Creating a date column
+    df_conversions = df_conversions.withColumn('date', lit(f'{year}-{month}-{day}'))
+
+    # Save DataFrame in parquet
+   
+   # local file and type
+    file_local = path
+    file_type = 'parquet'
+
+    # mode
+    mode = 'overwrite'
+
+    # saving
+    df_conversions.write.format(file_type)\
+        .mode(mode)\
+        .save(file_local)
 
 # COMMAND ----------
 
-year, month, day = extrat_data('2022-09-07', base = 'BRL')['date'].split('-')
-print(year, month, day)
-
-# COMMAND ----------
-
-dbutils.fs.ls('dbfs:/')
-
-# COMMAND ----------
-
-path = f'/Workspace/Repos/202307063411@alunos.estacio.br/Pipelines_with_Airflow_and_Azure-Databricks/data/bronze/{year}/{month}/{day}'
-path
-
-# COMMAND ----------
-
-def data_for_dataframe(data_json):
-    data_tuple = [(coin, float(rate)) for coin, rate in data_json['rates'].items()]
-    return data_tuple
-
-# COMMAND ----------
-
-response = data_for_dataframe(extrat_data('2022-09-07', base = 'BRL'))
-
-# COMMAND ----------
-
-spark.createDataFrame(response, schema = ['coin', 'rate']).show(n = 5)
-
-# COMMAND ----------
-
-df_conversions = spark.createDataFrame(response, schema = ['coin', 'rate'])
-
-# COMMAND ----------
-
-from pyspark.sql.functions import lit
-df_conversions = df_conversions.withColumn('date', lit(f'{year}-{month}-{day}'))
-df_conversions.limit(5).display()
-
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ## Save DataFrame
-
-# COMMAND ----------
-
-# local file and type
-file_local = path
-file_type = 'parquet'
-
-# mode
-mode = 'overwrite'
-
-# saving
-df_conversions.write.format(file_type)\
-    .mode(mode)\
-    .save(file_local)
+extracted_conversions = extrat_data(data_execucao, base = 'BRL')
+save_parquet(extracted_conversions)
 
 # COMMAND ----------
 
